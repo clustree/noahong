583a570975ceff42b8133e9c0b26c361a3414d65
Quentin Pradet <qpradet@csod.com>
2020-04-21 11:07:46 +0400

    Fix setup.py and cython-regenerate-noaho-setup.py

--- cython-regenerate-noaho-setup.py
+++ cython-regenerate-noaho-setup.py
@@ -1,14 +1,24 @@
-from distutils.core import setup
-from distutils.extension import Extension
+from setuptools import setup, Extension
+import sys
+
 from Cython.Distutils import build_ext
 
 # run with:
 # python cython-regenerate-noaho-setup.py build_ext --inplace
-# - the 'mv' is because pypi wants
 
-setup(ext_modules=[
-  Extension('noaho',               # name of extension
-            ['src/noaho.pyx',      # our Cython source
-             'src/array-aho.cpp'],
-            language='c++')],      # causes Cython to create C++ source
-      cmdclass={'build_ext': build_ext})
+extra_args = ["-std=c++11"]
+if sys.platform == "darwin":
+    extra_args.append("-stdlib=libc++")
+
+setup(
+    ext_modules=[
+        Extension(
+            "noahong",
+            ["src/noahong.pyx", "src/array-aho.cpp"],
+            language="c++",
+            extra_compile_args=extra_args,
+            extra_link_args=extra_args,
+        )
+    ],
+    cmdclass={"build_ext": build_ext},
+)
